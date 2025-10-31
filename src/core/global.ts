c2824542cd88a0926701f82e711881f618f159ef
kmou424 <me@kmou424.moe>
2025-11-01 04:01:26 +0800

    Revert "fix: add null checks for Global context setter and getter"
    This reverts commit 2926f622fde0d29153f3516713dedeefed5b22bb.

--- src/core/global.ts
+++ src/core/global.ts
@@ -1,19 +1,13 @@
 import { PluginContext } from "./type";
 
 class Global {
-  private static ctx: PluginContext | null = null;
+  private static ctx: PluginContext;
 
   public static set Context(ctx: PluginContext) {
-    if (Global.ctx) {
-      throw new Error("Context is already set");
-    }
     Global.ctx = ctx;
   }
 
   public static get Context() {
-    if (!Global.ctx) {
-      throw new Error("Context is not set");
-    }
     return Global.ctx;
   }
 }
