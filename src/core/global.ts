2926f622fde0d29153f3516713dedeefed5b22bb
kmou424 <me@kmou424.moe>
2025-10-31 21:19:45 +0800

    fix: add null checks for Global context setter and getter

--- src/core/global.ts
+++ src/core/global.ts
@@ -1,13 +1,19 @@
 import { PluginContext } from "./type";
 
 class Global {
-  private static ctx: PluginContext;
+  private static ctx: PluginContext | null = null;
 
   public static set Context(ctx: PluginContext) {
+    if (Global.ctx) {
+      throw new Error("Context is already set");
+    }
     Global.ctx = ctx;
   }
 
   public static get Context() {
+    if (!Global.ctx) {
+      throw new Error("Context is not set");
+    }
     return Global.ctx;
   }
 }
