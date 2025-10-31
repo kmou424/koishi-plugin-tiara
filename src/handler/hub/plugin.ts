bd7e8d4cdc861978685d31b8f7bfb1ce56c87e7a
kmou424 <me@kmou424.moe>
2025-11-01 03:55:32 +0800

    fix: upstream with createPluginContext changes

--- src/handler/hub/plugin.ts
+++ src/handler/hub/plugin.ts
@@ -1,12 +1,15 @@
 import { Context } from "koishi";
 import { Config } from "../../config";
-import { HandlerHub, PluginContext } from "../../core/type";
+import { createPluginContext } from "../../core/context";
+import { HandlerHub } from "../../core/type";
 import TiaraHandlerProvider from "../providers/command/tiara";
 import RevokeHandlerProvider from "../providers/message/revoke";
 
 export class PluginHandlerHub extends HandlerHub {
   Deploy(ctx: Context, config: Config) {
-    this.Providers().forEach((p) => p.Provide(PluginContext(ctx, config)));
+    this.Providers().forEach((p) =>
+      p.Provide(createPluginContext(ctx, config))
+    );
   }
 
   Providers = () => [new TiaraHandlerProvider(), new RevokeHandlerProvider()];
--- src/handler/hub/qq.ts
+++ src/handler/hub/qq.ts
@@ -1,13 +1,16 @@
 import { Context } from "koishi";
 import { Config } from "../../config";
-import { HandlerHub, MsgPlatform, PluginContext } from "../../core/type";
+import { createPluginContext } from "../../core/context";
+import { HandlerHub, MsgPlatform } from "../../core/type";
 import IsTruthHandlerProvider from "../providers/command/istruth";
 import MiniAppMessageHandlerProvider from "../providers/message/miniapp";
 
 export class QQHandlerHub extends HandlerHub {
   Deploy(ctx: Context, config: Config) {
     ctx = ctx.platform(...MsgPlatform.asKoishi(MsgPlatform.QQ));
-    this.Providers().forEach((p) => p.Provide(PluginContext(ctx, config)));
+    this.Providers().forEach((p) =>
+      p.Provide(createPluginContext(ctx, config))
+    );
   }
 
   Providers = () => [
